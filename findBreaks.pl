945f313db8af1dfc0fb88a1d044356e66ec5e17a
offmarc <offman@gmx.net>
2012-03-07 11:08:02 +0100

    links

--- findBreaks.pl
+++ findBreaks.pl
@@ -1,1 +1,49 @@
-/apps/scripts/script_repo/findBreaks.pl
+#!/usr/bin/perl -w
+
+
+###############
+##   Title   ##
+###############################################################################
+
+my $header = <<'EOHEADER';
+------------------------------------------------------------------------------
+		                findBreaks.pl	      
+------------------------------------------------------------------------------					      
+
+ A script to find gaps (missing residues) in PDB structures.
+									      
+		     Authors: (C) Marc Offman				      
+			       Date: 05.03.2012 			      
+			       Version: 1				      
+									      
+									      
+			  Thank you for using findBreaks.pl!			      
+	    Any questions should be adressed to offman@rostlab.org	      
+									      
+------------------------------------------------------------------------------
+
+EOHEADER
+
+open(PDB,"$ARGV[0]");
+$oldRes = -9999999;
+while(<PDB>){
+	chomp($_);
+	if(/^ATOM/){
+		#print "$_\n";
+		$res = substr($_,22,4);
+		$res=~s/\s+//g;
+		#print "|$res|\n";
+		if($oldRes != $res){
+			if($oldRes != -9999999 && abs($res-$oldRes) > 1 && $res > $oldRes){
+				print "$oldRes\:$res\n";
+			}
+			$oldRes = $res;
+		}
+	}
+	elsif(/^TER/){
+	
+	}
+}
+close PDB;
+#exit;
+
